751e3e1e91adb74b84f42f23d072b9941ea545ab
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2023-04-13 22:23:59 +0200

    feat(bic): New BIC type

---
+++ tests/BicTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Atournayre\Types\Tests;
+
+use Atournayre\Types\Bic;
+
+class BicTest extends \PHPUnit\Framework\TestCase
+{
+    public function testBicValidType(): void
+    {
+        $validBics = [
+            'ASPKAT2LXXX',
+            'ASPKAT2L',
+            'DSBACNBXSHA',
+            'UNCRIT2B912',
+            'DABADKKK',
+            'RZOOAT2L303',
+        ];
+
+        foreach ($validBics as $bic) {
+            try {
+                Bic::fromString($bic);
+                $this->assertTrue(true);
+            } catch (\InvalidArgumentException $e) {
+                throw $e;
+            }
+        }
+    }
+    public function testBicInvalidType(): void
+    {
+        $validBics = [
+            'ASPKAT2LXXX',
+            'ASPKAT2L',
+            'DSBACNBXSHA',
+            'UNCRIT2B912',
+            'DABADKKK',
+            'RZOOAT2L303',
+        ];
+
+        foreach ($validBics as $bic) {
+            try {
+                Bic::fromString($bic);
+                $this->assertTrue(true);
+            } catch (\InvalidArgumentException $e) {
+                throw $e;
+            }
+        }
+    }
+
+    public function testBICInvalidTypes()
+    {
+        $invalidBics = [
+            'DEUTD',
+            'ASPKAT2LXX',
+            'ASPKAT2LX',
+            'ASPKAT2LXXX1',
+            'DABADKK',
+            '1SBACNBXSHA',
+            'RZ00AT2L303',
+            'D2BACNBXSHA',
+            'DS3ACNBXSHA',
+            'DSB4CNBXSHA',
+            'DEUT12HH',
+            'DSBAC6BXSHA',
+            'DSBA5NBXSHA',
+            'DSBAAABXSHA',
+            'THISSVAL1D]',
+            'DEUTDEF]',
+            'DeutAT2LXXX',
+            'DEUTAT2lxxx',
+            'DEUTAT2l xx',
+            'DEUTAT2l?xx',
+        ];
+
+        foreach ($invalidBics as $bic) {
+            $this->expectException(\InvalidArgumentException::class);
+            $this->expectExceptionMessage(sprintf('Expected a value to contain letters and digits only. Got: "%s"', $bic));
+            Bic::fromString($bic);
+        }
+    }
+}
