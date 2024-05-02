aea5ab3942cb368ea144134bdb1fe997dfaa7576
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2024-05-02 09:42:40 +0200

    Add methods do EmailAddress: - is() - username() - usernameIs() - domain() - domainIs() - isDeliverable()

--- src/EmailAddress.php
+++ src/EmailAddress.php
@@ -12,4 +12,37 @@ class EmailAddress extends StringType
 
         $this->string = $email;
     }
+
+    public function is(string|self $email): bool
+    {
+        if ($email instanceof self) {
+            return $this->string === $email->toString();
+        }
+        return $this->string === $email;
+    }
+
+    public function username(): string
+    {
+        return explode('@', $this->string)[0];
+    }
+
+    public function usernameIs(string $username): bool
+    {
+        return $this->username() === $username;
+    }
+
+    public function domain(): string
+    {
+        return explode('@', $this->string)[1];
+    }
+
+    public function domainIs(string $domain): bool
+    {
+        return $this->domain() === $domain;
+    }
+
+    public function isDeliverable(): bool
+    {
+        return checkdnsrr($this->domain());
+    }
 }
